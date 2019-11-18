d0e1080c1fde9dd250062fbfebe223e4ddbcd5e4
Khai <khaitrinh99@gmail.com>
2019-11-18 10:55:42 -0800

    First commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "shipment.h"
+#include "semaphore.h"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
